ed4b259263c798d5e3c43a66674ba2845c2d8bed
Corvette Rox <development@thzero.com>
2025-10-06 15:11:06 -0500

    updates

--- src/communicationAltimeterData.h
+++ src/communicationAltimeterData.h
@@ -3,6 +3,15 @@
 
 #include "flightData.h"
 
+struct __attribute__((packed)) communicationMonitorDataStruct {
+  float cpuTemp = 0.0;
+  float cpuTempMax = 0.0;
+  int memoryHeap = 0;
+  int memoryRam = 0;
+  int memoryStack = 0;
+  float voltage = 0.0;
+};
+
 struct __attribute__((packed)) communicationExternalStruct {
   long diffTime;
   uint8_t state;
@@ -11,15 +20,6 @@ struct __attribute__((packed)) communicationExternalStruct {
   flightDataGPSValues gps;
   flightDataGyroscopeValues gyroscope;
   communicationMonitorDataStruct monitor;
-};
-
-struct __attribute__((packed)) communicationMonitorDataStruct {
-  float cpuTemp = 0.0;
-  float cpuTempMax = 0.0;
-  int memoryHeap = 0;
-  int memoryRam = 0;
-  int memoryStack = 0;
-  float voltage = 0.0;
 };
 
 struct __attribute__((packed)) communicationInternalStruct {
