00bc3bf9438077c74704b527d29d297fc5ebde73
Corvette Rox <development@thzero.com>
2025-11-06 07:50:34 -0600

    simulation complete refactor

--- src/simulation.h
+++ src/simulation.h
@@ -1,73 +1,108 @@
-// #ifndef _SIMULATION_H
-// #define _SIMULATION_H
+#ifndef _SIMULATION_H
+#define _SIMULATION_H
 
-// #include "loopThrottle.h";
-// #include "sensorData.h";
+#include <ArduinoJson.h>
 
-// #define SAMPLE_SIMULATION 1
+#include <loopThrottle.h>
+#include <sensorData.h>
+#include "stateMachine.h"
 
-// struct simulationConfig {
-//   double RocketMass = 1.76;                 // kg // from OR 3" rocket
-//   double MotorFuelBurnRate = 0.176;         // kg/s // from OR 3" rocket
-//   double MotorFuelBurnTime = 1.6;         // kg/s // from OR 3" rocket
-//   double MotorFuelMass = 0.316;             // kg // from OR 3" rocket
-//   double MotorThrust = 2485.796;            // m/s // from OR 3" rocket  
-//   double DragCoefficient = 0.87;            // from OR 3" rocket
-//   double FrontalArea = 4.56e-3;            // m^20.0762 // from OR 3" rocket
-//   int sampleRate = 100;
-// };
+#define SAMPLE_SIMULATION 1
 
-// // Very simple, and not very realistic, simulation only on the X and Y axis.
-// class simulation {
-//   public:
-//     simulation();
-//     bool isRunning();
-//     void outputSerialList();
-//     void start(int requestedNumber, long altitudeInitial, long altitudeFinal);
-//     void stop();
-//     accelerometerValues valueAcceleration();
-//     double valueAltitude();
+#define DEBUG_SIM_CONFIG
 
-//   private:
-//     double AirDensity = 1.292;        // kg/m^3
-//     double AirDensityScale = 5600.0;  // m -- height at which density is halved
-//     double EarthMass = 5.97e24;       // kg
-//     double EarthRadius = 6.3e6;       // m
-//     double GravConstant = 6.67e-11;   // m^3/kg/s^2;
-//     int SettleFinal = 100;
-//     int SettleStart = 15;
+struct simulationConfigStruct {
+  float cd = 0.87;     // from OR 3" rocket
+  float diameterIn = 5.5;
+  float diameterMM = 0;
+  float diameter = 0; // m  
+  float frontalArea = 4.56e-3;      // m^20.0762 // from OR 3" rocket
+  double mass = 1.76;          // kg // from OR 3" rocket
+  double motorFuelBurnRate = 0.176;  // kg/s // from OR 3" rocket
+  double motorFuelBurnRateMicro = 0.000000176;  // kg/s // from OR 3" rocket
+  double motorFuelBurnTime = 1.6;    // kg/s // from OR 3" rocket
+  double motorFuelMass = 0.316;      // kg // from OR 3" rocket
+  double motorThrust = 2485.796;     // m/s // from OR 3" rocket
+  float parachuteArea = 0;
+  float parachuteDiameterIn = 5.5;
+  float parachuteDiameterMM = 0;
+  float parachuteDiameter = 0; // m
+  float parachuteCd = 2.2;
+  int sampleRate = 100;
+};
+typedef enum {
+	simulationStatusAirborneAscent,
+	simulationStatusAirborneDescent,
+    simulationStatusGround,
+    simulationStatusLanded
+} simulationStatus;
 
-//     bool _airborne = false;
-//     bool _airborneApogee = false;
-//     double _altitudeApogee;
-//     double _altitudeApogeeMeasures;
-//     double _altitudeEnding;
-//     double _altitudeFlightLast;
-//     double _altitudeInitial;
-//     double _altitudeStarting;
-//     double _burnoutTime;  // s
-//     simulationConfig _config;
-//     float _elapsedTime;
-//     double _motorThrust;  // N
-//     bool _running = false;
-//     int _settleFinal;
-//     double _trace[3];
+// Very simple, and not very realistic, simulation only on the X and Y axis.
+class simulation {
+  public:
+    simulation();
+    bool isRunning();
+    void outputSerialList();
+    int8_t setup(JsonDocument settings);
+    void simulationTask();
+    void simulationTaskEnd();
+    void simulationTaskStart();
+    void simulationTaskWrapper();
+    void start(int requestedNumber, long altitudeInitial, long altitudeFinal);
+    void stop();
+    accelerometerValues valueAcceleration();
+    double valueAltitude();
+
+  private:
+    double AirDensity = 1.292;        // kg/m^3
+    double AirDensityScale = 5600.0;  // m -- height at which density is halved
+    double EarthMass = 5.97e24;       // kg
+    double EarthRadius = 6.3e6;       // m
+    double GravConstant = 9.8;          // m/s^2
+    int SettleFinal = 15;
+    int SettleStart = 15;
+
+    double _altitude;
+    double _acceleration;
+    bool _airborneApogee = false;
+    double _altitudeApogee;
+    double _altitudeApogeeMeasures;
+    double _altitudeEnding;
+    double _altitudeFlightLast;
+    double _altitudeInitial;
+    double _altitudeStarting;
+    unsigned long _burnoutTime; // microseconds
+    simulationConfigStruct _config;
+    JsonDocument _configs;
+    unsigned long _elapsedTime;
+    double _forceDrag;
+    double _forceGravity;
+    double _forceTotal;
+    double _motorThrust;  // N
+    bool _running = false;
+    simulationStatus _status;
+    int _settleFinal;
+    double _velocity;
+
+    unsigned long _start = 0;
+    int _count = 0;
+    int _countHeader = 0;
+    int _settle = 0;
     
-//     double _maxAltitude;
-//     double _maxVelocity;
-//     unsigned long _simulationTimestamp;
-//     loopThrottle _simulationThrottle;
-//     TaskHandle_t simulationTaskHandle;
+    double _maxAltitude;
+    double _maxVelocity;
+    unsigned long _simulationTimestamp; // microseconds
+    loopThrottle _simulationThrottle;
 
-//     void evaluateTimestep(float deltaT, double drag, double Mr, double Me, double G, double Ft, double *x);
-//     void outputPrint(float delta, double mass, double thrust, double altitude, double airDensity, double drag, bool burnout, double apogeeDelta);
-//     void outputPrintHeader();
-//     void loopStep(float deltaT, bool output, bool outputHeader);
-//     void simulationTask();
-//     static void simulationTaskW(void * parameter);
-// };
+    void evaluateTimestep(unsigned long deltaT, double drag, double Mr, double Ft);
+    void outputPrint(unsigned long delta, double mass, double thrust, double altitude, double airDensity, double drag, bool burnout, double apogeeDelta);
+    void outputPrintHeader();
+    void loopStep(unsigned long deltaT, bool output, bool outputHeader);
+};
 
-// extern simulation _simulation;
-// extern simulationConfig simulationConfigDefault;
+extern simulation _simulation;
+extern simulationConfigStruct simulationConfigDefault;
 
-// #endif
+extern int8_t setupSimulation(JsonDocument settings);
+
+#endif
