bc340a2a81c49625e17fb7b6677d8768193973fb
Corvette Rox <development@thzero.com>
2025-11-30 20:44:47 -0600

    updates

--- src/sensors.h
+++ src/sensors.h
@@ -10,15 +10,14 @@ extern sensorsSetupResults setupSensorsStart();
 class sensors: public sensorsBase {
   public:
     sensorsSetupResults setup(sensorBarometer* atmosphereSensor, sensorIMU* imuSensor, sensorGPS* gps, uint8_t calibrationBarometerId = 0, uint8_t calibrationBarometerStatusId = 0, uint8_t calibrationIMUId = 0, uint8_t calibrationIMUStatusId = 0) override;
-    int8_t initOverride(SensorOverrideFunctionPtr atmosphereFuncOverride = nullptr, SensorOverrideFunctionPtr imuFuncOverride = nullptr);
     void calibrationResetCommand(uint8_t* commandBuffer, uint16_t commandBufferLength) override;
     void readAccelerometer(sensorValuesStruct* data) override;
-    void readAtmosphere(sensorValuesStruct* data, unsigned long current, unsigned long delta) override;
     void readAtmosphere(sensorValuesStruct* data) override;
+    void readAtmosphere(sensorValuesStruct* data, unsigned long current, unsigned long delta) override;
     void readGps(sensorValuesStruct* data) override;
+    void readGyroscope(sensorValuesStruct* data) override;
     void readImu(sensorValuesStruct* data, unsigned long current, unsigned long delta) override;
     void readMagnetometer(sensorValuesStruct* data) override;
-    void readGyroscope(sensorValuesStruct* data) override;
 };
 
 extern sensors _sensors;
